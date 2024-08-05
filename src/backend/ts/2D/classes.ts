8d98cf86a7f8da0ae3c320609f9572405798db68
mubasshirMurshed <mubmurshed@gmail.com>
2024-08-06 00:41:52 +1000

    Added equals method to Point for easy comparing. Added ccw sorting functionality based on some primative zip method. JS just does not have a great sort function. Changed contains method to include and added contains to see if the point is a vertex as well. Added some help functions too.

--- src/backend/ts/2D/classes.ts
+++ src/backend/ts/2D/classes.ts
@@ -1,3 +1,5 @@
+type Pair<A, B> = [A, B];
+
 class Point2D {
     private _x: number;
     private _y: number;
@@ -26,6 +28,10 @@ class Point2D {
     public toList(): number[] {
         return [this._x, this._y];
     }
+
+    public equals(point: Point2D): boolean {
+        return this._x == point.x && this._y == point.y;
+    }
 }
 
 interface Edge {
@@ -38,8 +44,11 @@ interface Edge {
 class Polygon2D {
     private _vertices: Point2D[];
 
-    constructor(vertices: Point2D[]) {
+    constructor(vertices: Point2D[], requiresSort?: boolean) {
         this._vertices = [...vertices];
+        if (requiresSort) {
+            this._sortCCW();
+        }
     }
 
     /**
@@ -50,8 +59,52 @@ class Polygon2D {
 		return [...this._vertices];
 	}
 
-    private _sortCW() {
-        return
+    public getMeanVertex() {
+        var x: number = 0;
+        var y: number = 0;
+        for (var point of this._vertices) {
+            x += point.x;
+            y += point.y;
+        }
+        return new Point2D(x, y);
+    }
+
+    private _sortCCW() {
+        // Obtain centre point to reference from
+        var meanVertex: Point2D = this.getMeanVertex();
+
+        // Get angle from positive x-axis of each point
+        var angles: number[] = this._vertices.map((point) => {
+            // Get angle point makes with reference to shifted polygon to origin 
+            var angle = Math.atan2(point.y - meanVertex.y, point.x - meanVertex.x);
+
+            // Convert domain from (-pi, pi] to [0, 2*pi)
+            if (angle < 0) {
+                angle += 2*Math.PI;
+            }
+            return angle;
+        })
+
+        // Zip the points with key value of angle
+        var collection: Pair<Point2D, number>[] = [];
+        for (var i = 0; i < this._vertices.length; i++) {
+            collection.push([this._vertices[i], angles[i]]);
+        }
+
+        // Sort based on key
+        var sortedCollection = collection.sort((a, b) => {
+            if (a[1] < b[1]) {
+                return -1;
+            } else if (a[1] == b[1]) {
+                return 0;
+            } else {
+                return 1
+            }
+        });
+
+        return sortedCollection.map((value) => {
+            return value[0];
+        })
     }
 
     public getNumVertices(): number {
@@ -73,6 +126,15 @@ class Polygon2D {
     }
 
     public contains(testPoint: Point2D): boolean {
+        for (var point of this._vertices) {
+            if (testPoint.equals(point)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public includes(testPoint: Point2D): boolean {
         var flag1: boolean = false;
         var flag2: boolean = false;
         var p: Point2D;
@@ -106,6 +168,13 @@ class Polygon2D {
 
         // Both tests must return same true for point to be considered inside the polygon
         return flag1 || flag2;
+    }
+
+    public getEdge(idx: number): Edge {
+        return {
+            p: this._vertices[idx],
+            q: this._vertices[(idx + 1) % this._vertices.length]
+        };
     }
 }
 
