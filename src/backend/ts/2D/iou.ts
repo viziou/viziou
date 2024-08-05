24d06b080f5bdc8ceb13abcd8cd18341fc0c3bee
mubasshirMurshed <mubmurshed@gmail.com>
2024-08-06 00:42:23 +1000

    Added intersection over union pseudocode for 2D polygons IOU-8

---
+++ src/backend/ts/2D/iou.ts
@@ -0,0 +1,66 @@
+import { Point2D, Edge, Polygon2D } from './classes.ts';
+import { intersect } from 'mathjs';
+
+function getIntersectionPolygon(polygon1: Polygon2D, polygon2: Polygon2D): Polygon2D {
+    // Initialise point list for final polygon
+    var points: Point2D[] = [];
+
+    // Find all the points from polygon1 inside polygon2
+    for (var point of polygon1.vertices) {
+        if (polygon2.includes(point)) {
+            points.push(point);
+        }
+    }
+
+    // Find all the points from polygon2 inside polygon1
+    for (var point of polygon2.vertices) {
+        // Additional check to not include point if it is also a vertex of polygon1
+        if (!polygon1.contains(point) && polygon1.includes(point)) {
+            points.push(point);
+        }
+    }
+
+    // For all edge pairs see if there is any valid intersection
+    for (var i = 0; i < polygon1.getNumVertices(); i++) {
+        for (var j = 0; j < polygon2.getNumVertices(); j++) {
+            var pointInt = findIntersection(polygon1.getEdge(i), polygon2.getEdge(j));
+            if (pointInt) {
+                points.push(pointInt);
+            }
+        }
+    }
+
+    // Create new Polygon object
+    return new Polygon2D(points, true);
+}
+
+function findIntersection(edge1: Edge, edge2: Edge): Point2D | null {
+    // Get intersection point
+    var intersectionPoint = intersect(edge1.p.toList(), edge1.q.toList(), edge2.p.toList(), edge2.q.toList());
+    
+    // Process point whether valid or not
+    if (intersectionPoint === null) {
+        // None or infinite solutions
+        // In the case of overlapping, such points would have been found in the polygon inclusion test
+        return null;
+    } else {
+        var pointInt = new Point2D(Number(intersectionPoint[0]), Number(intersectionPoint[1]));
+        // Check bounds for elligible point
+        if (onEdge(edge1, pointInt) && onEdge(edge2, pointInt)) {
+            return pointInt;
+        } else {
+            return null;
+        }
+    }
+}
+
+function onEdge(edge: Edge, point: Point2D): boolean {
+    return (point.x >= edge.p.x && point.x < edge.q.x) || (point.x > edge.q.x && point.x <= edge.p.x);
+}
+
+function IoU(polygon1: Polygon2D, polygon2: Polygon2D): number {
+    var areaOfIntersection: number = getIntersectionPolygon(polygon1, polygon2).calculateArea();
+    return areaOfIntersection / (polygon1.calculateArea() + polygon2.calculateArea() - areaOfIntersection);
+}
+
+export { IoU };
