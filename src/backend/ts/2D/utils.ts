9bac7877f4e25dcdd41ebc64fcd84d8e455c80a5
mubasshirMurshed <mubmurshed@gmail.com>
2024-08-13 13:39:10 +1000

    Updated nearlyEquals to fix the relative error with epsilon. Using Number.MIN_VALUE did not translate to what it should be doing on line 10, hence it was removed IOU-8

--- src/backend/ts/2D/utils.ts
+++ src/backend/ts/2D/utils.ts
@@ -7,7 +7,7 @@ export function nearlyEqual(a: number, b: number, epsilon: number = 1e-10): bool
     if (a == b) {
         return true;
     } else if (a == 0 || b == 0 || (absA + absB < Number.MIN_VALUE)) {
-        return diff < (epsilon * Number.MIN_VALUE);
+        return diff < epsilon;
 	} else {
         return diff / Math.min((absA + absB), Number.MAX_VALUE) < epsilon;
     }
