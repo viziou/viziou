3009bc2dfedb81f249f8d75da7b6b519f3970fca
Speyedr <38199900+Speyedr@users.noreply.github.com>
2024-08-14 23:09:28 +1000

    Support for generators within iterators when performing Mocha tests, as well as a "square area" test, which passed perfectly on my machine.
    Note that I haven't used `nearlyEqual` here. To my surprise, the answers are exactly the same, however this may have to be changed to approximately equal later.

--- src/backend/ts/2D/fuzzy.spec.ts
+++ src/backend/ts/2D/fuzzy.spec.ts
@@ -2,6 +2,8 @@ import { describe, it} from "mocha";
 import { should as setupShould } from "chai";
 import { Edge, Point2D, Polygon2D } from "./classes.ts";
 import { getIntersectionPolygon, IoU } from "./iou.ts";
+import { abs } from 'mathjs'
+import { nearlyEqual } from './utils.ts'
 const should = setupShould();
 
 // Fuzzy
@@ -22,9 +24,31 @@ describe("Fuzzing Tests", () => {
   })
 
   describe("Expanding Square Test", () => {
-    it("should follow f(x) = x^2 curve")
+    // Generate an array of tests (lazy)
+    const MAX_SIZE = 1000
+    const STEP_SIZE = 0.01
 
-  })
+    function* generateTests() {
+      for (let i = 0; i < MAX_SIZE; i += STEP_SIZE) {
+        yield {'polygon': new Polygon2D([
+            new Point2D(0, 0),
+            new Point2D(i, 0),
+            new Point2D(i, i),
+            new Point2D(0, i)
+          ]),
+          'expectedArea': i**2}
+      }
+    }
+
+    const generator = generateTests()
+
+    it("should follow f(x) = x^2 curve", () => {
+    for (const {polygon, expectedArea} of generator) {
+      const calculated = polygon.calculateArea();
+      calculated.should.be.equal(expectedArea);
+      }
+    });
+  });
 
   describe("Expanding Triangle Test", () => {
     it("should follow f(x) = (x^2)/2 curve")
