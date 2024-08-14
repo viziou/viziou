d327180c32fc01893afce76ed1f1f5b9e544296f
Speyedr <38199900+Speyedr@users.noreply.github.com>
2024-08-14 17:56:45 +1000

    Added scaffolding / ideas for fuzzy testing 2D backend

--- src/backend/ts/2D/fuzzy.spec.ts
+++ src/backend/ts/2D/fuzzy.spec.ts
@@ -5,3 +5,35 @@ import { getIntersectionPolygon, IoU } from "./iou.ts";
 const should = setupShould();
 
 // Fuzzy
+describe("Fuzzing Tests", () => {
+  describe("Random Polygon Translation Transitive", () => {
+    it("should have the same area every time")
+
+  })
+
+  describe("Random Polygon Rotation Transitive", () => {
+    it("should have the same area every time")
+
+  })
+
+  describe("Random Polygon Translation + Rotation Transitive", () => {
+    it("should have the same area every time")
+
+  })
+
+  describe("Expanding Square Test", () => {
+    it("should follow f(x) = x^2 curve")
+
+  })
+
+  describe("Expanding Triangle Test", () => {
+    it("should follow f(x) = (x^2)/2 curve")
+
+  })
+
+  describe("Circle PI Test", () => {
+    it("should approximately equal pi")
+
+  })
+
+})
