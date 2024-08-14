c8e44586beee502ff31e16d08a64c6dccc5f4989
Speyedr <38199900+Speyedr@users.noreply.github.com>
2024-08-14 17:15:07 +1000

    Testing coupling front-end to back-end through middleware API. Area calculation seems reasonable in performance? Note that as it stands, the vertices have to be sorted every time they get passed to the area calculation.

--- src/backend/Interface.ts
+++ src/backend/Interface.ts
@@ -1,2 +1,22 @@
 /* API that is exposed to the front-end. */
+import { PolygonData } from '../utils/types';
+import { Point2D, Polygon2D } from './ts/2D/classes'
 
+class Backend2D {
+
+  static area( { geometry }: PolygonData ) {
+    const geometryPosition = geometry.getAttribute('position');
+    const vertices: Point2D[] = [];
+    for (let i = 0, l = geometryPosition.count; i < l; i+=3 ) {
+        vertices.push(new Point2D(geometryPosition.array[i], geometryPosition.array[i + 1]));
+    }
+    //console.log(geometryPosition);
+    //console.log(vertices)
+
+    // create a backend polygon
+    const polygon = new Polygon2D(vertices, true)
+    return polygon.calculateArea()
+  }
+}
+
+export { Backend2D };
