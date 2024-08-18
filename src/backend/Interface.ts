f2527c4d849ec42b488326bdcec8dae6b757ae83
Speyedr <38199900+Speyedr@users.noreply.github.com>
2024-08-18 15:58:04 +1000

    updated IoU interface to export the shape of intersection as well as area.

--- src/backend/Interface.ts
+++ src/backend/Interface.ts
@@ -1,32 +1,32 @@
 /* API that is exposed to the front-end. */
 import { PolygonData } from '../utils/types';
 import { Point2D, Polygon2D } from './ts/2D/classes';
-import { IoU } from './ts/2D/iou';
+import { IoU, getIntersectionPolygon } from './ts/2D/iou';
 import { BufferGeometry } from 'three';
 
 class Backend2D {
 
-  static area( { geometry }: PolygonData ) {
+  public static area( { geometry }: PolygonData ) {
     return this._threeGeometryToPolygon2D(geometry).calculateArea();
   }
 
-  static pointInPolygon({x, y}: { x: number; y: number }, { geometry, position }: PolygonData) {
+  public static pointInPolygon({x, y}: { x: number; y: number }, { geometry, position }: PolygonData) {
     const point = new Point2D(x, y);
     const offset = new Point2D(position[0], position[1]);
     const polygon = this._threeGeometryToPolygon2D(geometry).translate(offset);
     return polygon.includes(point);
   }
 
-  static IoU({ geometry: geometryA, position: positionA }: PolygonData,
+  public static IoU({ geometry: geometryA, position: positionA }: PolygonData,
              { geometry: geometryB, position: positionB }: PolygonData) {
     const offsetA = new Point2D(positionA[0], positionA[1]);
     const offsetB = new Point2D(positionB[0], positionB[1]);
     const polygonA = this._threeGeometryToPolygon2D(geometryA).translate(offsetA);
     const polygonB = this._threeGeometryToPolygon2D(geometryB).translate(offsetB);
-    return IoU(polygonA, polygonB);
+    return {area: IoU(polygonA, polygonB), shape: getIntersectionPolygon(polygonA, polygonB) };
   }
 
-  static centreOfMass({geometry, position}: PolygonData): {x: number; y: number} {
+  public static centreOfMass({geometry, position}: PolygonData): {x: number; y: number} {
     const offset = new Point2D(position[0], position[1]);
     return this._threeGeometryToPolygon2D(geometry).getCentroid().translate(offset).xy;
   }
