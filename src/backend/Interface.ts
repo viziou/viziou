9621a59f2bd9ded8f74070443a4fd9675f46bdf4
Speyedr <38199900+Speyedr@users.noreply.github.com>
2024-08-18 17:00:32 +1000

    Middleware should not return a backend data type since this data is going to the front-end, which work with THREE geometries. I decided not to return `PolygonData` since that would require setting up a `colour` attribute as well.

--- src/backend/Interface.ts
+++ src/backend/Interface.ts
@@ -2,7 +2,8 @@
 import { PolygonData } from '../utils/types';
 import { Point2D, Polygon2D } from './ts/2D/classes';
 import { IoU, getIntersectionPolygon } from './ts/2D/iou';
-import { BufferGeometry } from 'three';
+import { BufferGeometry, Vector3 } from 'three';
+import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry.js';
 
 class Backend2D {
 
@@ -23,7 +24,8 @@ class Backend2D {
     const offsetB = new Point2D(positionB[0], positionB[1]);
     const polygonA = this._threeGeometryToPolygon2D(geometryA).translate(offsetA);
     const polygonB = this._threeGeometryToPolygon2D(geometryB).translate(offsetB);
-    return {area: IoU(polygonA, polygonB), shape: getIntersectionPolygon(polygonA, polygonB) };
+    return {area: IoU(polygonA, polygonB),
+      shape: this._polygon2DtoThreeGeometry(getIntersectionPolygon(polygonA, polygonB)) };
   }
 
   public static centreOfMass({geometry, position}: PolygonData): {x: number; y: number} {
@@ -38,6 +40,14 @@ class Backend2D {
       vertices.push(new Point2D(geometryPosition.array[i], geometryPosition.array[i + 1]));
     }
     return new Polygon2D(vertices, true);
+  }
+
+  private static _polygon2DtoThreeGeometry(polygon: Polygon2D): BufferGeometry  {
+    const vertices: Vector3[] = [];
+    polygon.vertices.forEach(({x, y}) => {
+      vertices.push(new Vector3(x, y, 0));
+    });
+    return new ConvexGeometry(vertices);
   }
 }
 
