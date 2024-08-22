b0655cb71274d0f83efb5319c5701cabd0c81cfe
Satya Jhaveri <satyajhaveri@protonmail.ch>
2024-08-22 22:38:37 +1000

    Fixed 2D drag and drop functionality
    Still has typescript error in Polygon.tsx, and has bug where dragging an overlapping region drags multiple polygons

--- src/components/Polygon.tsx
+++ src/components/Polygon.tsx
@@ -1,83 +1,49 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useRef } from "react";
 import * as THREE from "three";
 import { PolygonData } from "../utils/types";
 import { PolygonContext } from "../contexts/PolygonContext";
 import { useThree } from "@react-three/fiber";
+import { DragControls } from "@react-three/drei";
 
 type PolygonProps = PolygonData & { index: number };
 
 const Polygon = ({ position, geometry, colour, index }: PolygonProps) => {
   const mesh = useRef<THREE.Mesh>(null);
   const { dispatch } = useContext(PolygonContext)!;
-    const [isDragging, setIsDragging] = useState(false);
-    const { camera, gl, viewport } = useThree();
-    const [initialOffset, setInitialOffset] = useState<[number, number] | null>(null);
+  const { camera, gl } = useThree();
+  const controlsRef = useRef<THREE.DragControls>(null);
 
-    const handleMouseDown = (event: THREE.PointerEvent) => {
-      if (event.button !== 2) {
-          return;
-      }
-      event.stopPropagation();
-      setIsDragging(true);
-
-      // convert initial mouse down position to world coordinates:
-      const normalisedMouseCoordinates=  new THREE.Vector2( (event.clientX / gl.domElement.clientWidth) * 2 - 1, -(event.clientY / gl.domElement.clientHeight)*2 + 1);
-      const worldCoordinates = new THREE.Vector3(normalisedMouseCoordinates.x, normalisedMouseCoordinates.y, 0)//.unproject(camera);
-
-      // set the initial offset:
-      if (mesh.current) {
-          setInitialOffset([
-              worldCoordinates.x - mesh.current.position.x,
-              worldCoordinates.y - mesh.current.position.y,
-          ]);
-      }
-    };
-
-    const handleMouseMove = (event: THREE.PointerEvent) => {
-      if (isDragging && initialOffset) {
-        event.stopPropagation();
-        if (mesh.current) {
-          const normalisedMouseCoordinates=  new THREE.Vector2( (event.clientX / gl.domElement.clientWidth) * 2 - 1, -(event.clientY / gl.domElement.clientHeight)*2 + 1);
-          const worldCoordinates = new THREE.Vector3(normalisedMouseCoordinates.x, normalisedMouseCoordinates.y, 0)//.unproject(camera);
-
-          // adjust the scale based on the camera's current zoom level:
-          const scaleX = viewport.width / gl.domElement.clientWidth;
-          const scaleY = viewport.height / gl.domElement.clientHeight;
-
-          const newPosition: [number, number] = [
-              (worldCoordinates.x - initialOffset[0]) * scaleX * 100,
-              (worldCoordinates.y - initialOffset[1]) * scaleY * 100,
-          ];
-
-          // Update mesh position:
-          mesh.current.position.set(...newPosition, 0);
-
-          // update Polygon array state:
-          if (dispatch) {
-            dispatch({ type: "UPDATE_POSITION", index: index, position: newPosition });
-          }
-        }
-      }
-    };
-
-    const handleMouseUp = (event: THREE.PointerEvent) => {
-      event.stopPropagation();
-      setIsDragging(false);
-      setInitialOffset(null);
-    };
+  const handleDragEnd = () => {
+    if (mesh.current) {
+      const v = new THREE.Vector3();
+      mesh.current.getWorldPosition(v);
+      const position = v.toArray().slice(0, 2) as [number, number];
+      if (dispatch)
+        dispatch({
+          type: "UPDATE_POSITION",
+          index: index,
+          position: position,
+        });
+    }
+  };
 
   return (
-    <mesh
-      position={[position[0], position[1], 0]}
-      geometry={geometry}
-      ref={mesh}
-        onPointerDown={handleMouseDown}
-        onPointerMove={handleMouseMove}
-        onPointerUp={handleMouseUp}
-        onPointerLeave={handleMouseUp} // treat leaving the polygon as stopping the click
-    >
-      <meshBasicMaterial color={colour} />
-    </mesh>
+    <>
+      <DragControls
+        ref={controlsRef}
+        args={[[mesh.current], camera, gl.domElement]}
+        transformGroup={true}
+        onDragEnd={handleDragEnd}
+      >
+        <mesh
+          position={[position[0], position[1], 0]}
+          geometry={geometry}
+          ref={mesh}
+        >
+          <meshBasicMaterial color={colour} />
+        </mesh>
+      </DragControls>
+    </>
   );
 };
 
