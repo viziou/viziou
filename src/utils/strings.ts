02f6e547b77d6470a5931725053354977cb92839
Speyedr <38199900+Speyedr@users.noreply.github.com>
2024-08-23 23:28:18 +1000

    Preparing for immediate pull request to merge export functionality to assist in debugging IoU implementation on a different branch.

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -2,11 +2,11 @@
 //  return String.fromCharCode.apply(null, new Uint8Array(buf));
 //}
 
-function str2ab(str: string) {
-  const buf = new ArrayBuffer(str.length*2); // 2 bytes per character
-  const bufView = new Uint16Array(buf);
-  for (let i=0, strLen=str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
+// function str2ab(str: string) {
+//   const buf = new ArrayBuffer(str.length*2); // 2 bytes per character
+//   const bufView = new Uint16Array(buf);
+//   for (let i=0, strLen=str.length; i < strLen; i++) {
+//     bufView[i] = str.charCodeAt(i);
+//   }
+//   return buf;
+// }
