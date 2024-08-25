02182077cf0f4578a90a5e379287e8e11ecdd899
Speyedr <38199900+Speyedr@users.noreply.github.com>
2024-08-25 22:41:24 +1000

    Added Drag + Drop shape translation for 2D Canvas (#74)
    * Added left-click + drag to pan 2D canvas
    
    Previous iteration disabled left click functionality and relied on right click to pan
    
    * Drag and Drop in progress
    
    Currently not correctly functional
    
    * Fixed 2D drag and drop functionality
    
    Still has typescript error in Polygon.tsx, and has bug where dragging an overlapping region drags multiple polygons
    
    * After a *ridiculous* amount of research and testing, it was discovered that the usage of `DragControls` was incorrect. Specifically, drei's `DragControls` does not map to Three.js' `DragControls` like initially expected. It was understood that anything in the R3F ecosystem is just a wrapper for a Three.js method with the same name, however, for some reason this isn't the case for `DragControls`.
    
    Committing now just so I can checkout the old version and see if there's any performance loss.
    
    * Confirmed by checking out the old version that there was indeed some amount of "lag" / resistance when dragging the shapes across the canvas, and that my suspicions are mostly placebo.
    
    Cleaned up code to (hopefully) make TypeScript happy.
    
    * Rebased 'main' onto this branch. Currently the little dot in the center doesn't drag with unless you explicitly put the mouse on the center point but that doesn't really matter as it's just for testing / debugging anyway.
    
    ---------
    
    Co-authored-by: Satya Jhaveri <satyajhaveri@protonmail.ch>

--- src/components/Polygon.tsx
+++ src/components/Polygon.tsx
@@ -1,15 +1,71 @@
-import { useRef } from 'react';
-import * as THREE from 'three';
-import { PolygonData } from '../utils/types';
+import { useContext, useRef } from "react";
+import * as THREE from "three";
+import { PolygonData } from "../utils/types";
+import { PolygonContext } from "../contexts/PolygonContext";
+import { DragControls } from "@react-three/drei";
 
-const Polygon = ({ position, geometry, colour }: PolygonData) => {
-    const mesh = useRef<THREE.Mesh>(null);
+type PolygonProps = PolygonData & { index: number };
 
-    return (
-        <mesh position={[position[0], position[1], 0]} geometry={geometry} ref={mesh}>
-            <meshBasicMaterial color={colour} />
+// TODO: Make information on top of the polygon as a child instead?
+
+const Polygon = ({ position, geometry, colour, index }: PolygonProps) => {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const { dispatch } = useContext(PolygonContext)!;
+  const originalPosition = useRef<[number, number]>([0, 0]);
+
+  const matrix = new THREE.Matrix4();
+
+  const handleDragEnd = () => {
+    /* Could trigger updates to IoU or something here maybe */
+  };
+
+  const handleDragStart = () => {
+    if (mesh.current) {
+      const v = new THREE.Vector3();
+      mesh.current.getWorldPosition(v)
+      originalPosition.current = v.toArray().slice(0,2) as [number, number];
+      //console.log('original_position: ', originalPosition);
+    }
+  };
+
+  const handleDrag = (localMatrix: THREE.Matrix4) => {
+    // TODO: research if there's a faster way to decompose
+    if (mesh.current) {
+      const new_v = new THREE.Vector3();
+      localMatrix.decompose(new_v, new THREE.Quaternion(), new THREE.Vector3)
+      const new_pos = new_v.toArray().slice(0,2) as [number, number]
+      //console.log('original position (inside handleDrag): ', originalPosition);
+      new_pos[0] += originalPosition.current[0];
+      new_pos[1] += originalPosition.current[1]; // TODO: this is ugly
+      if (dispatch) {
+        dispatch({
+          type: "UPDATE_POSITION",
+          index: index,
+          position: new_pos,
+        })
+      }
+    }
+  };
+
+  return (
+    <>
+      <DragControls
+        matrix={matrix}
+        autoTransform={false}
+        onDragStart={handleDragStart}
+        onDrag={(localMatrix) => {handleDrag(localMatrix)}}
+        onDragEnd={handleDragEnd}
+      >
+        <mesh
+          position={[position[0], position[1], 0]}
+          geometry={geometry}
+          ref={mesh}
+        >
+          <meshBasicMaterial color={colour} />
         </mesh>
-    );
+      </DragControls>
+    </>
+  );
 };
 
 export default Polygon;
--- src/components/Scene2D.tsx
+++ src/components/Scene2D.tsx
@@ -1,24 +1,29 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { Scene2DProps } from '../utils/types';
-import Polygon from './Polygon';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Scene2DProps } from "../utils/types";
+import Polygon from "./Polygon";
 
 const Scene2D = ({ polygons }: Scene2DProps) => {
-    return (
-        <Canvas style={{ height: "80vh", background: "#cccccc" }} >
+  return (
+    <Canvas style={{ height: "80vh", background: "#cccccc" }}>
+      {polygons.map((polygon, index) => (
+        <Polygon
+          key={index}
+          index={index}
+          position={polygon.position}
+          geometry={polygon.geometry}
+          colour={polygon.colour}
+        />
+      ))}
 
-            {polygons.map((polygon, index) => (
-                <Polygon key={index} position={polygon.position} geometry={polygon.geometry} colour={polygon.colour} />
-            ))}
-      
-            <OrbitControls
-                enableRotate={false} 
-                enablePan={true}
-                enableZoom={true} 
-            />
-
-        </Canvas>
-    );
+      <OrbitControls
+        enableRotate={false}
+        enablePan={true}
+        enableZoom={true}
+        enableDamping={false}
+      />
+    </Canvas>
+  );
 };
 
 export default Scene2D;
