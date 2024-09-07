9afa92e27ae7ea1c3983964eafcbda40305fa9e6
Satya Jhaveri <satyajhaveri@protonmail.ch>
2024-09-07 16:07:08 +1000

    Setup events for dragging to resize and rotate

--- src/components/Polygon.tsx
+++ src/components/Polygon.tsx
@@ -1,8 +1,9 @@
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState, useEffect, useMemo } from "react";
 import * as THREE from "three";
 import { PolygonData } from "../utils/types";
 import { PolygonContext } from "../contexts/PolygonContext";
 import { DragControls } from "@react-three/drei";
+import { useThree, ThreeEvent } from "@react-three/fiber";
 
 type PolygonProps = PolygonData & { index: number; selectable: boolean };
 
@@ -14,6 +15,8 @@ const Polygon = ({ position, geometry, colour, index, selectable }: PolygonProps
     useContext(PolygonContext)!;
   const originalPosition = useRef<[number, number]>([0, 0]);
   const matrix = new THREE.Matrix4();
+  const [boundingBox, setBoundingBox] = useState<THREE.Box3 | null>(null);
+  const { scene } = useThree();
 
   const selectPolygon = () => {
     if (!selectable) return;
@@ -69,6 +72,128 @@ const Polygon = ({ position, geometry, colour, index, selectable }: PolygonProps
     // console.log('mouse over', currentlyMousedOverPolygons)
   };
 
+  
+  //! RESIZE FUNCTIONS:
+  const [resizing, setResizing] = useState(false);
+
+  const handleResizeStart = (corner: string, event: ThreeEvent<MouseEvent>) => {
+    setResizing(true);
+    // todo:
+    console.log("resize start");
+  }
+
+  const handleResizeDrag = (event: ThreeEvent<MouseEvent>) => {
+    if (!resizing) return;
+    // todo:
+    console.log("resize dragging");
+  }
+
+  const handleResizeEnd = (event: ThreeEvent<MouseEvent>) => {
+    setResizing(false);
+    // todo:
+    console.log("resize ended");
+  }
+
+  //! ROTATE FUNCTIONS:
+  const [rotating, setRotating] = useState(false);
+
+  const handleRotateStart = (event: ThreeEvent<MouseEvent>) => {
+    setRotating(true);
+    // todo:
+    console.log("rotate start");
+  }
+
+  const handleRotateDrag = (event: ThreeEvent<MouseEvent>) => {
+    if (!rotating) return;
+    // todo:
+    console.log("rotate dragging");
+  }
+
+  const handleRotateEnd = (event: ThreeEvent<MouseEvent>) => {
+    setRotating(false);
+    // todo:
+    console.log("rotate ended");
+  }
+
+
+
+  // bounding box component:
+  const BoundingBox = useMemo(() => {
+    if (!boundingBox || !isPolygonSelected()) return null;
+
+    const size = boundingBox.getSize(new THREE.Vector3());
+    const center = boundingBox.getCenter(new THREE.Vector3());
+
+    return (
+      <group position={center}>
+        {/* Red Lines to show bounding box: */}
+        <line>
+          <bufferGeometry>
+            <bufferAttribute
+              attach="attributes-position"
+              count={5}
+              array={new Float32Array([
+                -size.x/2, -size.y/2, 0,
+                size.x/2, -size.y/2, 0,
+                size.x/2, size.y/2, 0,
+                -size.x/2, size.y/2, 0,
+                -size.x/2, -size.y/2, 0
+              ])}
+              itemSize={3}
+            />
+          </bufferGeometry>
+          <lineBasicMaterial color="red" />
+        </line>
+        {/* Boxes on each corner: */}
+        {['topLeft', 'topRight', 'bottomLeft', 'bottomRight'].map((corner, i) => (
+          <mesh
+            key={corner}
+            position={[
+              (i % 2 === 0 ? -1 : 1) * size.x / 2,
+              (i < 2 ? 1 : -1) * size.y / 2,
+              0
+            ]}
+            onPointerDown={(e) => {handleResizeStart(corner, e);}}
+
+            onPointerMove={handleResizeDrag}
+
+            onPointerUp={handleResizeEnd}
+          >
+            <boxGeometry args={[0.2, 0.2, 0]} />
+            <meshBasicMaterial color="blue" />
+          </mesh>
+        ))}
+        {/* Rotate circle: */}
+        <mesh
+          position={[0, size.y / 2 + 0.5, 0]}
+          onPointerDown={handleRotateStart}
+
+          onPointerMove={handleRotateDrag}
+
+          onPointerUp={handleRotateEnd}
+        >
+          <sphereGeometry args={[0.1, 16, 16]} />
+          <meshBasicMaterial color="green" />
+        </mesh>
+        {/* Line to Rotate circle: */}
+        <line>
+          <bufferGeometry>
+            <bufferAttribute
+              attach="attributes-position"
+              count={2}
+              array={new Float32Array([
+                0, size.y / 2, 0,
+                0, size.y / 2 + 0.5, 0,
+              ])}
+              itemSize={3}
+            />
+          </bufferGeometry>
+          <lineBasicMaterial color="red" />
+        </line>
+      </group>
+    );
+  }, [boundingBox, isPolygonSelected, scene]);
+
   return (
     <>
       <DragControls
@@ -95,10 +220,11 @@ const Polygon = ({ position, geometry, colour, index, selectable }: PolygonProps
           onClick={selectPolygon}
         >
           <meshBasicMaterial
-            color={selectedPolygonIndex === index ? "red" : colour}
+            color={colour}
           />
         </mesh>
       </DragControls>
+      {BoundingBox}
     </>
   );
 };
